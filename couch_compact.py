db3132e7e088d0cbb4e8ef65a8d2502ba7edf579
Brian Hatfield <bhatfield@brightcove.com>
2012-08-27 22:29:31 -0400

    Make everything in the script optional

--- couch_compact.py
+++ couch_compact.py
@@ -1,9 +1,18 @@
 import json
 import urllib2
+import datetime
+from optparse import OptionParser
 
-couch_server = "192.168.33.11"
-couch_port = 5984
-db = "chef"
+
+def save_url(url, file_handle, size=4096):
+    req = urllib2.urlopen(url)
+
+    while True:
+        chunk = req.read(size)
+        if chunk:
+            file_handle.write(chunk)
+        else:
+            break
 
 
 def post(url):
@@ -18,13 +27,32 @@ def views(url):
     return [x['id'].split("/")[1] for x in designs['rows']]
 
 
-# Retrieve all the 'views', and then compact each one.
-views_URL = 'http://%s:%s/%s/_all_docs?startkey="_design/"&endkey="_design0"' % (couch_server, couch_port, db)
-for view in views(views_URL):
-    print view, post("http://%s:%s/%s/_compact/%s" % (couch_server, couch_port, db, view))
+parser = OptionParser()
+parser.add_option("--couch-server", dest="server", default="192.168.33.11", help="Couch Database Server Hostname/IP")
+parser.add_option("--couch-port", dest="port", default=5984, help="Couch Database Server Port")
+parser.add_option("--database", dest="database", default='chef', help="Database to work with")
 
-# Run a 'view cleanup' against all the views that have just been compacted.
-print "view_cleanup", post("http://%s:%s/%s/_view_cleanup" % (couch_server, couch_port, db))
+parser.add_option("--backup", dest="backup", action='store_true', default=False, help="Export database backup")
+parser.add_option("--compact-views", dest="compact_views", action='store_true', default=False, help="Compact each view")
+parser.add_option("--compact-database", dest="compact_database", action='store_true', default=False, help="Compact database")
+parser.add_option("--cleanup-views", dest="cleanup_views", action='store_true', default=False, help="Clean up previously-compacted view files")
+(options, args) = parser.parse_args()
 
-# Finally, run a compaction against the whole database.
-print "compact", post("http://%s:%s/%s/_compact" % (couch_server, couch_port, db))
+if options.compact_views:
+    # Retrieve all the 'views', and then compact each one.
+    views_URL = 'http://%s:%s/%s/_all_docs?startkey="_design/"&endkey="_design0"' % (options.server, options.port, options.database)
+    for view in views(views_URL):
+        print view, post("http://%s:%s/%s/_compact/%s" % (options.server, options.port, options.database, view))
+
+if options.cleanup_views:
+    # Run a 'view cleanup' against all the views that have just been compacted.
+    print "view_cleanup", post("http://%s:%s/%s/_view_cleanup" % (options.server, options.port, options.database))
+
+if options.compact_database:
+    # Run a compaction against the whole database.
+    print "compact", post("http://%s:%s/%s/_compact" % (options.server, options.port, options.database))
+
+if options.backup:
+    d = datetime.datetime.now()
+    with open('backup-%s-%s.bak' % (options.database, d.strftime("%Y-%m-%d_%H.%M.%S")), 'w') as handle:
+        save_url("http://%s:%s/%s/_all_docs?include_docs=true" % (options.server, options.port, options.database), handle)
