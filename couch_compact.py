af174b119a71ec39fb6a28ca91d22c54309c5889
Brian Hatfield <bhatfield@brightcove.com>
2012-08-27 22:32:27 -0400

    Add 'all' option to shortcut running each type of compaction individually

--- couch_compact.py
+++ couch_compact.py
@@ -32,27 +32,28 @@ parser.add_option("--couch-server", dest="server", default="192.168.33.11", help
 parser.add_option("--couch-port", dest="port", default=5984, help="Couch Database Server Port")
 parser.add_option("--database", dest="database", default='chef', help="Database to work with")
 
+parser.add_option("--all", dest="all", action='store_true', default=False, help="Run all compaction and backup steps")
 parser.add_option("--backup", dest="backup", action='store_true', default=False, help="Export database backup")
 parser.add_option("--compact-views", dest="compact_views", action='store_true', default=False, help="Compact each view")
 parser.add_option("--compact-database", dest="compact_database", action='store_true', default=False, help="Compact database")
 parser.add_option("--cleanup-views", dest="cleanup_views", action='store_true', default=False, help="Clean up previously-compacted view files")
 (options, args) = parser.parse_args()
 
-if options.compact_views:
+if options.compact_views or options.all:
     # Retrieve all the 'views', and then compact each one.
     views_URL = 'http://%s:%s/%s/_all_docs?startkey="_design/"&endkey="_design0"' % (options.server, options.port, options.database)
     for view in views(views_URL):
         print view, post("http://%s:%s/%s/_compact/%s" % (options.server, options.port, options.database, view))
 
-if options.cleanup_views:
+if options.cleanup_views or options.all:
     # Run a 'view cleanup' against all the views that have just been compacted.
     print "view_cleanup", post("http://%s:%s/%s/_view_cleanup" % (options.server, options.port, options.database))
 
-if options.compact_database:
+if options.compact_database or options.all:
     # Run a compaction against the whole database.
     print "compact", post("http://%s:%s/%s/_compact" % (options.server, options.port, options.database))
 
-if options.backup:
+if options.backup or options.all:
     d = datetime.datetime.now()
     with open('backup-%s-%s.bak' % (options.database, d.strftime("%Y-%m-%d_%H.%M.%S")), 'w') as handle:
         save_url("http://%s:%s/%s/_all_docs?include_docs=true" % (options.server, options.port, options.database), handle)
